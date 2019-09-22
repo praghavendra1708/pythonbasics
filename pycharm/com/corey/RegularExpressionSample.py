34273d497555c49cc4de845d0dacc1ab8fe50ea8
praghavendra1708 <raghavendra.pr.reddy@gmail.com>
2019-09-22 11:40:53 +0530

    adding regular expression

--- pycharm/com/corey/RegularExpressionSample.py
+++ pycharm/com/corey/RegularExpressionSample.py
@@ -1,9 +1,10 @@
 import re
+from re import Match
 
 print(r'\tTab')
 
 text_to_search = '''
-abcdefghijklmnopqurtuvwxyz
+abcdefghijklmnopqurtuvwxyz word
 ABCDEFGHIJKLMNOPQRSTUVWXYZ
 1234567890
 Ha HaHa
@@ -22,5 +23,35 @@ Mrs. Robinson
 Mr. T
 '''
 
+def matchfun(mypattern):
+    global text_to_search
+    patInterator = mypattern.finditer(text_to_search)
+    for x in patInterator:
+        print(x)
 
-pattern = re.compile('abc')
+
+pattern = re.compile(r'\b\D+?\s==')
+matchfun(pattern)
+
+
+
+# Matches phone number
+phonePattern = re.compile(r'\b\d{3}[\.|-]\d{3}[\.|-]\d{4}\b')
+print("Seaching for phone pattern")
+matchfun(phonePattern)
+
+
+
+
+# Seraching single back slash pattern
+backSlashPatter = re.compile(r'\\')
+backSlashPatter = re.compile('\\\\')
+
+print(backSlashPatter.findall(r'\\\\\\'))
+
+backSlashPatterWithRaw = re.compile(r'\\')
+print(backSlashPatterWithRaw.findall('\\\\\\'))
+
+
+
+
